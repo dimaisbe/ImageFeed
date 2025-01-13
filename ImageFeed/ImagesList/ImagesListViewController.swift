db86769fb4c15dd032e7bb61a589f9f9a0703b78
dimaisbe <prinzipg@mail.ru>
2025-01-14 02:48:41 +0300

    no message

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,14 +1,14 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Дмитрий Железняков on 30.12.2024.
-//
-
 import UIKit
 
 final class ImagesListViewController: UIViewController {
+    
+    // MARK: - @IBOutlet properties
+    
     @IBOutlet private var tableView: UITableView!
+    
+    // MARK: - properties
+    
+    private let currentDate = Date()
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)"}
     
@@ -22,9 +22,7 @@ final class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-        
     }
 }
 
@@ -36,7 +34,10 @@ extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
 
-        guard let imageListCell = cell as? ImagesListCell else {
+        guard let imageListCell = tableView.dequeueReusableCell(
+            withIdentifier: ImagesListCell.reuseIdentifier,
+            for: indexPath
+        ) as? ImagesListCell else {
             return UITableViewCell()
         }
 
@@ -53,7 +54,7 @@ extension ImagesListViewController {
         }
 
         cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.dateLabel.text = dateFormatter.string(from: currentDate)
 
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
@@ -62,7 +63,9 @@ extension ImagesListViewController {
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: - Добавить логику при нажатии на ячейку
+    }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
